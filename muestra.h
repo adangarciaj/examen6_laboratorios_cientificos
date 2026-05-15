5a90d4012e6624f0ee7d2459be9429f72ace9bee
adangarciaj <adaptar2812@gmail.com>
2026-05-15 13:19:41 -0600

    Agregue el typedef, define arreglos y clave de las muestras

---
+++ muestra.h
@@ -0,0 +1,13 @@
+#ifndef MUESTRA_H
+#define MUESTRA_H
+
+#define MAX_NOMBRE 50
+#define MAX_DEP 50
+
+typedef struct {
+    int clave;
+    char nombre[MAX_NOMBRE];
+    char departamento[MAX_DEP];
+}Muestra;
+
+#endif 
